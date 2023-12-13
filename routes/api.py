96ea9d700d1e2f0910054121b682665888f5a1df
SoInstant <44108612+SoInstant@users.noreply.github.com>
2023-12-13 19:20:44 +0800

    iwt eat

--- routes/api.py
+++ routes/api.py
@@ -3,6 +3,7 @@ from flask import Blueprint, request, session, abort
 
 from routes.admin import authenticate
 from database import db
+from utils import shortname_from_id
 
 
 api_blueprint = Blueprint("api", __name__, url_prefix="/api")
@@ -34,8 +35,46 @@ def check(username: str):
     results = list(
         col.find({"customer.username": username}, {"paid": 0, "splitting": 0})
     )
-    return results
 
+    data = {
+        "url": None,
+        "pin": None,
+        "authorised": {},
+        "transactions": [],
+        "legend": {},
+    }
+    if len(results) == 0:
+        return data
+
+    for r in results:  # each r is a transaction
+        # from each transaction: require date + money +
+        # what is bought (doc_id, shortname and chapters)
+        t2 = []
+        for item in r["sold"]:
+            _id = str(item["doc_id"])
+            chapters = item["chapters"]
+            if _id not in data["legend"]:
+                data["legend"][_id] = shortname_from_id(_id)
+            if _id not in data["authorised"]:
+                data["authorised"][_id] = []
+            data["authorised"][_id].extend(chapters)
+
+            t2.append(
+                {
+                    "id": _id,
+                    "chapters": chapters,
+                }
+            )
+
+        t = {
+            "date": r["date"].timestamp(),
+            "money": r["money"],
+            "bought": t2,
+        }
+        data["transactions"].append(t)
+    data["url"] = results[0]["customer"]["url"]
+    data["pin"] = results[0]["customer"]["pin"]
+    return data
 
 @api_blueprint.post("/update")
 @requires_admin
--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from bson import ObjectId
 
 @cached(cache=TTLCache(maxsize=10, ttl=60))
 def shortname_from_id(id):
-    return db.documents.find_one({"_id": ObjectId(id)})["name"]
+    return db.documents.find_one({"_id": ObjectId(id)})["shortname"]
 
 
 def generate_passphase():
@@ -37,36 +37,3 @@ def create_user():
             "display_name": input("Name: "),
         }
     )
-
-
-def check(username: str):
-    col = db.transactions
-    results = list(
-        col.find({"customer.username": username}, {"paid": 0, "splitting": 0})
-    )
-
-    data = {"url": None, "pin": None, "authorised": [], "transactions": []}
-    if len(results) == 0:
-        return data
-
-    for r in results:  # each r is a transaction
-        # from each transaction: require date + money +
-        # what is bought (doc_id, shortname and chapters)
-        data["transactions"].append(
-            {
-                "date": r["date"],
-                "money": r["money"],
-                "bought": [
-                    {
-                        "id": item["doc_id"],
-                        "shortname": shortname_from_id(item["doc_id"]),
-                        "chapters": item["chapters"],
-                    }
-                    for item in r["sold"]
-                ],
-            }
-        )
-    return data
-
-
-print(check("jeff bozos"))
